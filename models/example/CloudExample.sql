ec8d0d690a5e195d43d474060b3a8d15d89fc5d1
Raju A <rajusugunakar@gmail.com>
2024-02-02 12:44:30 +0000

    New changes with ref function

--- models/example/CloudExample.sql
+++ models/example/CloudExample.sql
@@ -1,4 +1,4 @@
 with cte_amt as 
-(select Sum(Amount) as amt,category from COPY_DB.PUBLIC.ORDERS_EX group  by 2)
+(select Sum(Amount) as amt,category from {{ref('SourceOrders') }} group  by 2)
 
 select * from cte_amt
